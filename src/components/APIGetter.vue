84941b4672354404caf3385fdfa779d074371b7f
Andy chuang <bboy81905@outlook.com>
2018-09-20 09:48:15 +0800

    Add new finction in APIGetter: search and choose type

--- src/components/APIGetter.vue
+++ src/components/APIGetter.vue
@@ -2,9 +2,35 @@
   <div class="APIGetter">
     <h1>{{ msg }}</h1>
     <div class="row">
-      <div class="col-md-4 col-12 mb-2" v-for="statement in AirList" :key="statement.index">
+      <div v-if="!Loading&&AirList.length>0" class="alert alert-success" role="alert">
+
+        總共 {{AirList.length}} 筆資料
+
+      </div>
+
+      <div v-else-if="AirList.length<0" class="alert alert-danger" style="width:100%">
+        查無此資料
+      </div>
+      <div class="input-group">
+        <h3 class="mr-2">Type : </h3>
+        <select class="" id="inputGroupSelect" @change="optionChange($event)" v-model="type">
+          <option selected>All</option>
+          <option v-for="(option,index) in typeOption" :value=option :key="index">{{option}}</option>
+        </select>
+        <input type="text" class="form-control" placeholder="Search" v-model="keywords">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary" type="button" @click="SearchGo()">Go</button>
+        </div>
+      </div>
+
+      <div class="cardC col-md-4 col-12 mb-2" v-for="statement in AirList" :key="statement.index">
         <div class="card">
-          <h2 class="card-header">{{statement.Category}}</h2>
+          <div class="card-header">
+            <h2>{{statement.Category}}</h2>
+            <span class="badge badge-secondary">{{statement.Unit}}</span>
+
+          </div>
+
           <div class="card-body">
             <h3 class="card-title">{{statement.Title.slice(0,31)}}</h3>
             <p class="card-text">{{statement.Content.slice(0,50)}} .....</p>
@@ -14,6 +40,7 @@
       </div>
 
     </div>
+
   </div>
 </template>
 
@@ -23,27 +50,60 @@ export default {
   name: "APIGetter",
   data() {
     return {
-      msg: "APIGetter"
+      msg: "APIGetter",
+      typeOption: ["講堂", "研習", "展覽", "留學資訊", "其他"],
+      keywords:'',
+      type:'All'
     };
   },
   computed: {
     ...mapGetters({
-      AirList: "getAirList"
+      AirList: "getAirList",
+      Loading: "getLoading"
     })
   },
   methods: {
-    ...mapActions(["actionGetAPI"])
+    ...mapActions(["actionGetAPI", "typeChange",
+    "keywordsChange"]),
+    optionChange({ target: { value } }) {
+      //解構賦值
+      console.log(value);
+      this.typeChange(value);
+    },
+    SearchGo (){
+      this.keywordsChange(this.keywords)
+
+    }
   },
-  created() {this.actionGetAPI();},
-  mounted() {
-    
-  }
+  created() {
+    this.actionGetAPI();
+    this.typeChange(this.type);
+  },
+  mounted() {},
+ 
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h2{
-  font-weight: 700
+h2 {
+  font-weight: 700;
+}
+.cardC:nth-child(-n + 5) {
+  margin-top: 1rem;
+}
+.alert-success {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  z-index: 10;
+  animation: fadeinout 4s linear;
+  opacity: 0;
+}
+
+@keyframes fadeinout {
+  50% {
+    opacity: 1;
+  }
 }
 </style>
--- src/store/modules/API.js
+++ src/store/modules/API.js
@@ -1,17 +1,34 @@
 import axios from 'axios'
 import * as rootTypes from '../rootTypes'
 const types = {
-    GETAPILIST: 'GetAPI'
+    GETAPILIST: 'GetAPI',
+    TYPECHANGE: 'TYPECHANGE',
+    KEYWORDS:'KEYWORDS'
 }
 
 const state = {
-    AirList: ''
+    AirList: [],
+    type: "All",
+    keywords:''
 }
 
 const getters =
-{
-    getAirList: state => { return state.AirList }
-}
+    {
+        getAirList: state => {
+            let Opendata = state.AirList
+            if (state.type !== 'All') {
+                Opendata =  Opendata.filter(item=>item.Category===state.type)
+            }
+            if (state.type!==''){
+
+                Opendata = Opendata.filter(item=> JSON.stringify(item).indexOf(state.keywords)!=-1)
+
+
+            }
+            return Opendata;
+
+        }
+    }
 
 const actions = {
     actionGetAPI({ commit }) {
@@ -20,18 +37,33 @@ const actions = {
             .get('http://opendata.khcc.gov.tw/public/OD_ksml_news.ashx')
             .then(function (response) {
                 let data = response.data;
+                //console.log(data)
                 commit(types.GETAPILIST, data);
                 commit(rootTypes.LOADING, false);
             })
             .catch(function (error) {
                 console.log(error);
             });
+    },
+    typeChange({ commit }, val) {
+        commit(types.TYPECHANGE, val)
+
+    },
+    keywordsChange({commit},keywords){
+        commit(types.KEYWORDS,keywords)
+
     }
 }
 
 const mutations = {
-    [types.GETAPILIST](state,data) {
+    [types.GETAPILIST](state, data) {
         state.AirList = data
+    },
+    [types.TYPECHANGE](state, val) {
+        state.type = val
+    },
+    [types.KEYWORDS](state,keywords){
+        state.keywords = keywords
     }
 }
 
